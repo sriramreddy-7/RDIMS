1b11d9cf6d7a296045e700e5291c0d7b106789da
sriramreddy-7 <sriramreddykoonadi@gmail.com>
2023-07-12 12:03:29 +0530

    added runtime.txt

--- rdims/views.py
+++ rdims/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.response import Response
+# from rest_framework.response import Response
 # from rest_framework.decorators import api_view
 # from rest_framework.decorators import api_view, permission_classes
 # from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
