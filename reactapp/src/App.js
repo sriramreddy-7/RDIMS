65856110b6777ac217a0659704be564f3f14352d
sathwikreddy bolledla <115647430+sathwikreddybolledla@users.noreply.github.com>
2023-07-22 15:26:08 +0530

    Login 2

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,12 +2,22 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { Register } from "./components/Register";
+import { Login } from "./components/Login";
+import {useState} from 'react';
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
   return (
     <>
-      <div className="App"></div>
-      <Navbar title="Sriram Reddy" aboutText="IMS Portal" />
+      <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
+      </div>
     </>
   );
 }
